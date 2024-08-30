fc249ff0cef505c875481ea24a239a804997958b
Nuwan Adambarage <nuwansda@gmail.com>
2024-08-31 07:02:32 +1000

    Build expandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,33 @@
+import { Button, Text, Textarea } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+interface Props {
+  children: string;
+}
+
+const ExpandableText = ({ children }: Props) => {
+  const [expand, setExpand] = useState(false);
+  const limit = 300;
+
+  if (!children) return null;
+  if (children.length <= limit) return <Text>{children}</Text>;
+
+  const summary = expand ? children : children.substring(0, limit) + "...";
+
+  return (
+    <Text>
+      {summary}
+      <Button
+        marginLeft={1}
+        size="xs"
+        fontWeight="bold"
+        colorScheme="yellow"
+        onClick={() => setExpand(!expand)}
+      >
+        {expand ? "Show less" : "Read more"}
+      </Button>
+    </Text>
+  );
+};
+
+export default ExpandableText;
