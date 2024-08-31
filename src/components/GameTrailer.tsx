3d9671f3f5d477ed72e9285f9d3386c920419caf
Nuwan Adambarage <nuwansda@gmail.com>
2024-09-01 07:43:24 +1000

    Build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,20 @@
+import useTrailers from "../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useTrailers(gameId);
+
+  if (error) throw error;
+  if (isLoading) return null;
+
+  const first = data?.results[0];
+
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls />
+  ) : null;
+};
+
+export default GameTrailer;
